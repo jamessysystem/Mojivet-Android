c52faccd232fd03574ed08c032dfdf513b4cfb1e
Jamessysystem <smenardjames@gmail.com>
2024-02-22 23:08:24 +0800

    fixed fragment_appointment.xml
    Signed-off-by: Jamessysystem <smenardjames@gmail.com>

--- app/src/main/java/com/example/mojivet/nav_bottom/Appointment.kt
+++ app/src/main/java/com/example/mojivet/nav_bottom/Appointment.kt
@@ -3,9 +3,13 @@ package com.example.mojivet.nav_bottom
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import android.view.View
 import android.view.ViewGroup
 import com.example.mojivet.R
+import java.util.Calendar
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -18,18 +22,8 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class Appointment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
+    private lateinit var spinner: Spinner
+    private val calendar = Calendar.getInstance()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -37,24 +31,28 @@ class Appointment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_appointment, container, false)
     }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Appointment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Appointment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+        spinner = view.findViewById(R.id.spin_vet)
+
+
+        val choices = arrayOf("Veterinarian 1", "Veterinarian 2", "Veterinarian 3")
+
+        val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_spinner_item, choices)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = adapter
+
+        // Optionally, you can add a listener to handle spinner item selection
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                val selectedItem = parent?.getItemAtPosition(position).toString()
+                // Do something with the selected item
             }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                // Do something when nothing is selected
+            }
+        }
     }
 }
