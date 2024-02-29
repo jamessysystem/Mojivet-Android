150c6830a40dd4d544161950976b0143285d6ece
Jamessysystem <smenardjames@gmail.com>
2024-02-29 21:53:26 +0800

    Changed login,signup, and forgotpassword from fragments to activity.
    Signed-off-by: Jamessysystem <smenardjames@gmail.com>

--- app/src/main/java/com/example/mojivet/nav_drawer/Photos.kt
+++ app/src/main/java/com/example/mojivet/nav_drawer/Photos.kt
@@ -5,9 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView.ScaleType
-import com.denzcoskun.imageslider.ImageSlider
-import com.denzcoskun.imageslider.models.SlideModel
 import com.example.mojivet.R
 
 
@@ -19,15 +16,15 @@ class Photos : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_photos, container, false)
 
-        val imageSlider = view.findViewById<ImageSlider>(R.id.image_slider)
-        val imageList = ArrayList<SlideModel>()
+//        val imageSlider = view.findViewById<ImageSlider>(R.id.image_slider)
+//        val imageList = ArrayList<SlideModel>()
 
-        imageList.add(SlideModel("https://i.natgeofe.com/n/abb126f7-eed1-437b-9c40-760fda76032f/05-dog-portraits-FP-Jpegs-5_2x3.jpg","Asongot"))
-//        imageList.add(SlideModel("",""))
-//        imageList.add(SlideModel("",""))
-
-        imageSlider.setImageList(imageList, com.denzcoskun.imageslider.constants.ScaleTypes.FIT)
-        // Inflate the layout for this fragment
+//        imageList.add(SlideModel("https://i.natgeofe.com/n/abb126f7-eed1-437b-9c40-760fda76032f/05-dog-portraits-FP-Jpegs-5_2x3.jpg","Asongot"))
+////        imageList.add(SlideModel("",""))
+////        imageList.add(SlideModel("",""))
+//
+//        imageSlider.setImageList(imageList, com.denzcoskun.imageslider.constants.ScaleTypes.FIT)
+//        // Inflate the layout for this fragment
         return view
     }
 }
